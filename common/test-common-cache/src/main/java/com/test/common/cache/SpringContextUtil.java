93321d161b54bfa5a16f1cc027b140feef7079b0
maozebing <mzb0312@163.com>
2016-05-30 17:13:54 +0800

    1

---
+++ common/test-common-cache/src/main/java/com/test/common/cache/SpringContextUtil.java
@@ -0,0 +1,14 @@
+package com.test.common.cache;
+
+/**
+ * 描述 ：
+ *
+ * @author : mzb
+ * @version : v1.00
+ * @CreationDate : 16-5-30 下午5:13
+ * @Description :
+ * @update : 修改人，修改时间，修改内容
+ * @see :[相关类/方法]
+ */
+public class SpringContextUtil {
+}
